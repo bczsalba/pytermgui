edcba893e6eb5c8323caa37b65b2855895613035
bczsalba <bczsalba@gmail.com>
2021-04-04 17:42:46 +0200

    added `.interactive` submodule

--- pytermgui/__init__.py
+++ pytermgui/__init__.py
@@ -2,6 +2,7 @@ from .ui import *
 from .input import getch, InputField
 from . import utils
 
+
 color         = Color.color
 bold          = Color.bold
 italic        = Color.italic
@@ -13,3 +14,5 @@ strikethrough = Color.strikethrough
 
 # this is added in order to avoid padders hogging memory
 padding_label = Label()
+
+from .utils import interactive
