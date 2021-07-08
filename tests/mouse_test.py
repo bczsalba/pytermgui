dc05d60dd75b1a3c44c295555764073fbf1a6acc
bczsalba <bczsalba@gmail.com>
2021-07-09 00:37:01 +0200

    Improved tests/mouse_test.py

--- tests/mouse_test.py
+++ tests/mouse_test.py
@@ -10,38 +10,42 @@ This file serves to test basic mouse capabilities.
 
 from typing import Optional, Union
 
-from pytermgui import alt_buffer, mouse_handler, getch, foreground, ListView, Prompt, InputField, Splitter, Label, Container
+from pytermgui import (
+    alt_buffer,
+    mouse_handler,
+    getch,
+    foreground,
+    ListView,
+    Prompt,
+    InputField,
+    Splitter,
+    Label,
+    Container,
+    ColorPicker,
+    boxes,
+)
 
-
-def parse_mouse(code: str) -> Optional[Union[tuple[tuple[int], bool], Exception]]:
-    """Parse mouse codes"""
-
-    if not len(code) > 2 or not code[2] == "<":
-        return None, None
-
-    nums = code.split(";")[1:]
-    is_pressed = nums[1][-1] == "M"
-    nums[1] = nums[1][:-1]
-
-    try:
-        ints = [int(num) for num in nums]
-    except ValueError:
-        # this gets triggered when the parameter for `code` is
-        # not valid
-        return ValueError, False
-
-    ints[0] -= 1
-
-    return tuple(ints), is_pressed
 
 def show_targets(root: Container) -> None:
     """Debug all root mouse targets"""
 
     for widget in root:
-        for target in widget.mouse_targets: 
+        for target in widget.mouse_targets:
             target.show(103)
 
+    for target in root.mouse_targets:
+        target.show(103)
+
+
+def set_value(button: ListView, value: str) -> None:
+    """Set widget value"""
+
+    button.options = [value]
+
+
 with alt_buffer(echo=False, cursor=False), mouse_handler("press") as mouse:
+    boxes.DOUBLE.set_chars_of(Container)
+
     root = Label("[214 bold]ListView:").get_container()
     root += ListView(["first", "second", "third"])
     root += Label("[214 bold]Prompt:")
@@ -49,6 +53,19 @@ with alt_buffer(echo=False, cursor=False), mouse_handler("press") as mouse:
     root += Label("[214 bold]InputField:")
     root += InputField("hello\nthere")
     root += InputField("obiwan\nkenobi")
+
+    cp = ColorPicker(16)
+    cp += ListView(["hello"])
+    cp[-1].onclick = lambda target, widget: print("this shouldn't work!")
+
+    button = ListView(["background"])
+    button.onclick = lambda target, widget: (
+        cp.toggle_layer(),
+        set_value(widget, ("foreground" if cp.layer == 0 else "background")),
+    )
+
+    root += cp
+    root += button
 
     root.forced_width = 100
     root.center().print()
@@ -70,9 +87,7 @@ with alt_buffer(echo=False, cursor=False), mouse_handler("press") as mouse:
             pressed, pos = mouse_event
 
             if pressed:
-                # root.blur()
-
-                if not root.click(pos):
+                if root.click(pos) is None:
                     root.blur()
 
         root.print()
