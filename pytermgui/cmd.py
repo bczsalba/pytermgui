9615887ef34aae6c37adc4375937f9ed50108ac2
bczsalba <bczsalba@gmail.com>
2021-08-15 23:26:17 +0200

    Add background color demos to `cmd.py/MarkupApplication`

--- pytermgui/cmd.py
+++ pytermgui/cmd.py
@@ -263,6 +263,16 @@ class MarkupApplication(Application):
             Label("[{color}]rrr;ggg;bbb", parent_align=2).set_style(
                 "value", lambda _, item: ansi(item.format(color=self._rgb_color))
             ),
+            Label(),
+            Label("[@{color}]@0-255", parent_align=2).set_style(
+                "value", lambda _, item: ansi(item.format(color=self._255_color))
+            ),
+            Label("[@{color}]@#rrgggbb", parent_align=2).set_style(
+                "value", lambda _, item: ansi(item.format(color=self._hex_color))
+            ),
+            Label("[@{color}]@rrr;ggg;bbb", parent_align=2).set_style(
+                "value", lambda _, item: ansi(item.format(color=self._rgb_color))
+            ),
         ]
 
         corners = Container.chars["corner"].copy()
