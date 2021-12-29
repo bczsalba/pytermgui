99ab493e6b2a7dcf6fdcfb946e4e48e5eac065b5
bczsalba <bczsalba@gmail.com>
2021-12-29 19:36:30 +0100

    Document __init__.py
    This is the first doc commit in the long list to 1.0.0.

--- pytermgui/__init__.py
+++ pytermgui/__init__.py
@@ -4,7 +4,75 @@ pytermgui
 author: bczsalba
 
 
-A simple yet powerful TUI framework for your Python (3.9+) applications
+A simple yet powerful TUI framework for your Python (3.6+) applications.
+
+There is a couple of parts that make up this module, all building on top
+of eachother to achieve the final result.
+
+Your usage depends on which part of the library you use. I will provide an
+example for each.
+
+## Low level
+
+At the base, there is `ansi_interface` and `input`, handling terminal APIs
+for output and input respectively. This is the lowest-level layer of the
+library.
+
+```python3
+import pytermgui as ptg
+
+ptg.set_alt_buffer()
+print("This text will be discarded on '\n' input.")
+while ptg.getch() != "\n":
+    print("Wrong key.")
+ptg.unset_alt_buffer()
+```
+
+
+## Helper level
+
+On top of that, there is the helper layer, including things like `helpers`,
+`context_managers` and the kind. These provide no extra functionality, only
+combine functions defined below them in order to make them more usable.
+
+
+```python3
+import pytermgui as ptg
+
+for line in ptg.break_line(
+    "This is some \033[1mlong\033[0m and \033[38;5;141mstyled\033[0m text.",
+    limit=10,
+)
+```
+
+
+## High level
+
+Building on all that is the relatively high level `widgets` module. This
+part uses things from everything defined before it to create a visually
+appealing interface system. It introduces a lot of its own APIs and 
+abstractions, and leaves all the parts below it free of cross-layer
+dependencies.
+
+The highest layer of the library is for `window_manager`. This layer combines
+parts from everything below. It introduces abstractions on top of the `Widget`
+system, and creates its own featureset.
+
+
+```python3
+import pytermgui as ptg
+
+with ptg.WindowManager() as manager:
+    manager.add(
+        ptg.Window()
+        + "[141 bold]Title"
+        + "[grey italic]body text"
+        + ""
+        + ["Button"]
+    )
+
+    manager.run()
+```
 """
 
 # https://github.com/python/mypy/issues/4930
@@ -69,24 +137,30 @@ def auto(  # pylint: disable=R0911
 
     Currently supported:
         - Label (str):
-             + value: str   -> Label(value, **attrs)
+            + `"This becomes a label [141] with markup!"`
+            + value: str   -> Label(value, **attrs)
 
         - Splitter (tuple):
-             + tuple[item1: Any, item2: Any] -> Splitter(item1, item2, ..., **attrs)
+            + `("left", Container("1", "2"), "right")`
+            + tuple[item1: Any, item2: Any] -> Splitter(item1, item2, ..., **attrs)
 
-        - buttons (list):
-             + default:
-                 * list[label: str, onclick: ButtonCallback]  -> Button(label, onclick, **attrs)
+        - Various button types (list):
+            + Button:
+                * `["Button Label", lambda target, caller: input(caller)]`
+                * list[label: str, onclick: ButtonCallback]  -> Button(label, onclick, **attrs)
 
-             + Checkbox:
-                 * list[default: bool, callback: Callable[[bool], Any]]  ->
-                     Checkbox(default, callback, **attrs)
+            + Checkbox:
+                * `[True, lambda checked: input(checked)]`
+                * list[default: bool, callback: Callable[[bool], Any]]  -> \
+                        Checkbox(default, callback, **attrs)
 
-             + Toggle:
-                 * list[tuple[state1: str, state2: str], callback: Callable[[str], Any] ->
-                     Toggle((state1, state2), callback)
+            + Toggle:
+                * `[("Default", "Modified"), lambda new_value: input(new_value)]`
+                * list[tuple[state1: str, state2: str], callback: Callable[[str], Any] -> \
+                        Toggle((state1, state2), callback)
 
-        - prompt splitter:
+        - Splitter prompt:
+            + `{"key": InputField("value")}`
             + dict[left: Any, right: Any]  -> (
                 Splitter(
                     auto(left, parent_align=0),
@@ -95,12 +169,12 @@ def auto(  # pylint: disable=R0911
                 )
             )
 
-    If Widget.ALLOW_TYPE_CONVERSION is True (default), this method is
+    This method is
     called implicitly whenever a non-widget is attempted to be added to
-    a Widget.
+    a Widget. It returns None in case of a failure
 
     Example:
-        >>> from pytermgui import Container, get_widget, clear
+        >>> from pytermgui import Container
         >>> form = (
         ... Container(id="form")
         ... + "[157 bold]This is a title"
