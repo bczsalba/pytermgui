9d3bad78f238c717d0e7082873c10e510da2f039
bczsalba <bczsalba@gmail.com>
2021-12-30 19:15:29 +0100

    Document `input`

--- pytermgui/input.py
+++ pytermgui/input.py
@@ -6,9 +6,9 @@ author: bczsalba
 
 File providing the getch() function to easily read character inputs.
 
-credits:
-- original getch implementation: Danny Yoo (https://code.activestate.com/recipes/134892)
-- modern additions & idea:       kcsaff (https://github.com/kcsaff/getkey)
+Credits:
+- Original getch implementation: Danny Yoo (https://code.activestate.com/recipes/134892)
+- Modern additions & idea:       kcsaff (https://github.com/kcsaff/getkey)
 """
 
 # pylint doesn't see the C source
@@ -36,22 +36,22 @@ from codecs import getincrementaldecoder
 
 
 def _is_ready(file: IO[AnyStr]) -> bool:
-    """Return if file is reading for reading"""
+    """Determine if IO object is reading to read"""
 
     result = select([file], [], [], 0.0)
     return len(result[0]) > 0
 
 
 class _GetchUnix:
-    """Getch implementation for UNIX"""
+    """Getch implementation for UNIX systems"""
 
     def __init__(self) -> None:
-        """Set up instance attributes"""
+        """Initialize object"""
 
         self.decode = getincrementaldecoder(sys.stdin.encoding)().decode
 
     def _read(self, num: int) -> str:
-        """Read num characters from sys.stdin"""
+        """Read `num` characters from sys.stdin"""
 
         buff = ""
         while len(buff) < num:
@@ -64,7 +64,7 @@ class _GetchUnix:
         return buff
 
     def get_chars(self) -> Generator[str, None, None]:
-        """Get characters while possible, yield them"""
+        """Yield characters while there are some available"""
 
         descriptor = sys.stdin.fileno()
         old_settings = termios.tcgetattr(descriptor)
@@ -88,11 +88,11 @@ class _GetchUnix:
 
 
 class _GetchWindows:
-    """Getch implementation for UNIX"""
+    """Getch implementation for Windows"""
 
     @staticmethod
     def _ensure_str(string: AnyStr) -> str:
-        """Return str from bytes"""
+        """Ensure return value is always a `str` and not `bytes`"""
 
         if isinstance(string, bytes):
             return string.decode("utf-8")
@@ -100,7 +100,7 @@ class _GetchWindows:
         return string
 
     def get_chars(self) -> str:
-        """Read num characters from sys.stdin"""
+        """Read `num` characters from sys.stdin"""
 
         char = msvcrt.getch()
         if char == b"\xe0":
@@ -127,7 +127,7 @@ class _Keys:
     The keys for CTRL_{ascii_letter}-s can be generated with
     the following code:
 
-    ```
+    ```python3
     for i, letter in enumerate(ascii_lowercase):
         key = f"CTRL_{letter.upper()}"
         code = chr(i+1).encode('unicode_escape').decode('utf-8')
@@ -186,7 +186,7 @@ class _Keys:
                 self._keys[key] = code
 
     def __getattr__(self, attr: str) -> str:
-        """Overwrite __getattr__ to get from self._keys"""
+        """Get attr from self._keys"""
 
         if attr == "ANY_KEY":
             return attr
@@ -194,7 +194,7 @@ class _Keys:
         return self._keys[attr]
 
     def get_name(self, key: str, default: Optional[str] = None) -> Optional[str]:
-        """Get name of a key code"""
+        """Get canonical name of a key code"""
 
         for name, value in self._keys.items():
             if key == value:
@@ -273,7 +273,7 @@ except ImportError as import_error:
 
 
 def getch(printable: bool = False, interrupts: bool = True) -> Any:
-    """Wrapper for the getch functions"""
+    """Wrapper to call the platform-appropriate character getter"""
 
     try:
         key = _getch()
