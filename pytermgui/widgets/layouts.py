6a5ab8721c828f55f590dca07b148d4b1b629b2b
bczsalba <bczsalba@gmail.com>
2022-01-28 17:38:34 +0100

    Fix #24

--- pytermgui/widgets/layouts.py
+++ pytermgui/widgets/layouts.py
@@ -756,10 +756,12 @@ class Container(Widget):
             return True
 
         # Only use navigation when there is more than one selectable
-        if self.selectables_length > 1 and _is_nav(key):
+        if self.selectables_length >= 1 and _is_nav(key):
             handled = False
+
             if self.selected_index is None:
                 self.select(0)
+                return True
 
             assert isinstance(self.selected_index, int)
 
