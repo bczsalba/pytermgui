40c6d6684b0b7cc07bf68c767cafd1ac5c4733c0
bczsalba <bczsalba@gmail.com>
2022-05-09 11:14:16 +0200

    Add highlighter test
    Most of the submodule seems to already be covered by other parts, so
    for now we only needed to test markup literals. In the future this
    probably should be expanded, however.

---
+++ tests/test_highlight_markup_literal.py
@@ -0,0 +1,8 @@
+from pytermgui import highlight_python
+
+
+def test_literal_highlight():
+    assert (
+        highlight_python("'[This is a test]'")
+        == r"[code.str]'\[This is a test]'[/code.str]"
+    )
