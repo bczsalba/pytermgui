9c1f0667a85b7444ddaba0d77a4784a8363063e2
bczsalba <bczsalba@gmail.com>
2022-07-15 21:55:38 +0200

    Document `markup/language.py`

--- pytermgui/markup/language.py
+++ pytermgui/markup/language.py
@@ -1,12 +1,13 @@
+"""Wrappers around the TIM parsing engine, implementing caching and context management."""
+
 from __future__ import annotations
 
 from dataclasses import dataclass
-from io import StringIO
-from typing import Callable
+from typing import Callable, Generator, Iterator
 
 from ..colors import ColorSyntaxError, str_to_color
 from ..regex import RE_MACRO
-from ..terminal import terminal
+from ..terminal import get_terminal
 from .aliases import apply_default_aliases
 from .macros import apply_default_macros
 from .parsing import (
@@ -18,7 +19,8 @@ from .parsing import (
     tokenize_markup,
     tokens_to_markup,
 )
-from .tokens import PlainToken, Token
+from .style_maps import CLEARERS
+from .tokens import Token
 
 __all__ = [
     "MarkupLanguage",
@@ -26,8 +28,17 @@ __all__ = [
     "tim",
 ]
 
+Tokenizer = Callable[[str], Iterator[Token]]
+
 
 class MarkupLanguage:
+    """A relatively simple object that binds context to TIM parsing functions.
+
+    Most of the job this class has is to pass along a `ContextDict` to various
+    "lower level" functions, in order to maintain a sort of state. It also exposes
+    ways to modify this state, namely the `alias` and `define` methods.
+    """
+
     def __init__(
         self, *, default_aliases: bool = True, default_macros: bool = True
     ) -> None:
@@ -45,19 +56,54 @@ class MarkupLanguage:
 
     @property
     def aliases(self) -> dict[str, str]:
+        """Returns a copy of the aliases defined in context."""
+
         return self._aliases.copy()
 
     @property
     def macros(self) -> dict[str, Callable[[str, ...]]]:
+        """Returns a copy of the macros defined in context."""
+
         return self._macros.copy()
 
     def define(self, name: str, method: Callable[[str, ...], str]) -> None:
+        """Defines a markup macro.
+
+        Macros are essentially function bindings callable within markup. They can be
+        very useful to represent changing data and simplify TIM code.
+
+        Args:
+            name: The name that will be used within TIM to call the macro. Must start with
+                a bang (`!`).
+            method: The function bound to the name given above. This function will take
+                any number of strings as arguments, and return a terminal-ready (i.e. parsed)
+                string.
+        """
+
         if not name.startswith("!"):
-            name = f"!{name}"
+            raise ValueError("TIM macro names must be prefixed by `!`.")
 
         self._macros[name] = method
 
     def alias(self, name: str, value: str, *, generate_unsetter: bool = True) -> None:
+        """Creates an alias from one custom name to a set of styles.
+
+        These can be used to store and reference a set of tags using only one name.
+
+        Aliases may reference other aliases, but only do this consciously, as it can become
+        a hard to follow trail of sorrow very quickly!
+
+        Args:
+            name: The name this alias will be referenced by.
+            value: The markup value that the alias will represent.
+            generate_unsetter: Disable generating clearer aliases.
+                For example:
+                    my-tag = 141 bold italic
+
+                will generate:
+                    /my-tag = /fg /bold /italic
+        """
+
         value = eval_alias(value, self.context)
 
         def _generate_unsetter() -> str:
@@ -84,6 +130,22 @@ class MarkupLanguage:
     def alias_multiple(
         self, *, generate_unsetter: bool = True, **items: dict[str, str]
     ) -> None:
+        """Runs `MarkupLanguage.alias` repeatedly for all arguments.
+
+        The same `generate_unsetter` value will be used for all calls.
+
+        You can use this in two forms:
+
+        - Traditional keyword arguments:
+
+            lang.alias_multiple(my-tag1="bold", my-tag2="italic")
+
+        - Keyword argument unpacking:
+
+            my_aliases = {"my-tag1": "bold", "my-tag2": "italic"}
+            lang.alias_multiple(**my_aliases)
+        """
+
         for name, value in items.items():
             self.alias(name, value, generate_unsetter=generate_unsetter)
 
@@ -93,6 +155,18 @@ class MarkupLanguage:
         optimize: bool = False,
         append_reset: bool = True,
     ) -> str:
+        """Parses some markup text.
+
+        This is a thin wrapper around `pytermgui.markup.parsing.parse`. The main additions
+        of this wrapper are a caching system, as well as state management.
+
+        Ignoring caching, all calls to this function would be equivalent to:
+
+            def parse(self, *args, **kwargs) -> str:
+                kwargs["context"] = self.context
+
+                return parse(*args, **kwargs)
+        """
 
         key = (text, optimize, append_reset)
 
@@ -115,29 +189,53 @@ class MarkupLanguage:
         return output
 
     # TODO: This should be deprecated.
-    def get_markup(self, text: str) -> str:
+    @staticmethod
+    def get_markup(text: str) -> str:
+        """DEPRECATED: Convert ANSI text into markup.
+
+        This function does not use context, and thus is out of place here.
+        """
+
         return tokens_to_markup(tokenize_ansi(text))
 
-    # TODO: This should be deprecated.
-    def get_styled_plains(self, text: str) -> Generator[StyledText, None, None]:
-        yield from StyledText.group_styles(text)
+    def group_styles(
+        self, text: str, tokenizer: Tokenizer = tokenize_ansi
+    ) -> Generator[StyledText, None, None]:
+        """Generate StyledText-s from some text, using our context.
 
-    def print(
-        self,
-        *items,
-        sep: str = " ",
-        end: str = "\n",
-        flush: bool = True,
-        file: StringIO = terminal,
-    ) -> None:
-        buff = []
-        for item in items:
-            buff.append(self.parse(item))
+        See `StyledText.group_styles` for arguments.
+        """
 
-        file.write(sep.join(buff) + end)
+        yield from StyledText.group_styles(
+            text, tokenizer=tokenizer, context=self.context
+        )
 
-        if flush:
-            file.flush()
+    def prettify_markup(self, markup: str) -> str:
+        """Syntax-highlight markup."""
+
+        output = ""
+
+        for span in StyledText.group_styles(
+            markup, tokenizer=tokenize_markup, context=self.context
+        ):
+            tags = " ".join(token.prettified_markup for token in span.tokens[:-1])
+            markup = " ".join(tkn.markup for tkn in span.tokens[:-1])
+
+            if len(tags) > 0 and len(markup) > 0:
+                output += f"[{tags}][{markup}]"
+
+            output += f"{span.plain}[/]"
+
+        return self.parse(output, optimize=True)
+
+    def print(self, *args, **kwargs) -> None:
+        """Parse all arguments and pass them through to print, along with kwargs."""
+
+        parsed = []
+        for arg in args:
+            parsed.append(self.parse(str(arg)))
+
+        get_terminal().print(*parsed, **kwargs)
 
 
 tim = MarkupLanguage()
@@ -182,7 +280,11 @@ class StyledText:
     # inverse: bool
 
     @staticmethod
-    def group_styles(text: str) -> Generator[StyledText, None, None]:
+    def group_styles(
+        text: str,
+        tokenizer: Tokenizer = tokenize_ansi,
+        context: ContextDict | None = None,
+    ) -> Generator[StyledText, None, None]:
         """Yields StyledTexts from an ANSI coded string.
 
         A new StyledText will be created each time a non-plain token follows a
@@ -190,11 +292,16 @@ class StyledText:
         of characters.
         """
 
+        context = context if context is not None else ContextDict.create()
+
         parsers = PARSERS
         link = None
 
         def _parse(token: Token) -> str:
             nonlocal link
+
+            if token.is_macro():
+                return token.markup
 
             if token.is_hyperlink():
                 link = token
@@ -203,11 +310,15 @@ class StyledText:
             if link is not None and token.is_clear() and token.targets(link):
                 link = None
 
-            return parsers[type(token)](token, {})
+            if token.is_clear() and token.value not in CLEARERS:
+                return token.markup
+
+            return parsers[type(token)](token, context)
 
         tokens = []
+        token: Token
 
-        for token in tokenize_ansi(text):
+        for token in tokenizer(text):
             if token.is_plain():
                 yield StyledText(
                     "".join(_parse(tkn) for tkn in tokens),
@@ -215,20 +326,28 @@ class StyledText:
                     tokens + [token],
                     link.value if link is not None else None,
                 )
-                tokens = []
                 continue
+
+            if token.is_clear():
+                tokens = [tkn for tkn in tokens if not token.targets(tkn)]
+
+                if len(tokens) > 0 and tokens[-1] == token:
+                    continue
+
+            if len(tokens) > 0 and all(tkn.is_clear() for tkn in tokens):
+                tokens = []
 
             tokens.append(token)
 
-        if len(tokens) > 0:
-            token = PlainToken("")
+        # if len(tokens) > 0:
+        #     token = PlainToken("")
 
-            yield StyledText(
-                "".join(_parse(tkn) for tkn in tokens),
-                token.value,
-                tokens + [token],
-                link.value if link is not None else None,
-            )
+        #     yield StyledText(
+        #         "".join(_parse(tkn) for tkn in tokens),
+        #         token.value,
+        #         tokens + [token],
+        #         link.value if link is not None else None,
+        #     )
 
     @classmethod
     def first_of(cls, text: str) -> StyledText | None:
