5a4705b22b7a9982bcdec127e065f51c00d66209
bczsalba <bczsalba@gmail.com>
2022-08-13 18:42:09 +0200

    [tests]: Fix `test_highlight_markup_literal` to reflect recent change
    ...to (temporarily) remove `highlight_python` pre-formatting.

--- tests/test_highlight_markup_literal.py
+++ tests/test_highlight_markup_literal.py
@@ -4,5 +4,5 @@ from pytermgui import highlight_python
 def test_literal_highlight():
     assert (
         highlight_python("'[This is a test]'")
-        == r"[code.str]'\[This is a test]'[/code.str]"
+        == r"[code.str]'[This is a test]'[/code.str]"
     )
