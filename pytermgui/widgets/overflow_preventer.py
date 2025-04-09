97b82473646fb0adb12436a65211b2b86989b577
keala <K13DOHERTY@student.bridgew.edu>
2025-04-09 19:05:29 -0400

    attempt #5 to implement fix for group issue one for overflow_preventer.py

--- pytermgui/widgets/overflow_preventer.py
+++ pytermgui/widgets/overflow_preventer.py
@@ -1,6 +1,6 @@
 
 from pytermgui import *
 
-def overflow_preventer(c = container.height,w = window.height ):
+def overflow_preventer(c = container.height, w = window.height ):
         if(c > w):
             raise ValueError("container size is too big and has overflown Window please reconfigure Container size")
